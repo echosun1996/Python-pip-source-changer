4c61a26737587402d2abb8959fc91c3539f4274f
Echo <echosun1996@126.com>
2018-05-23 23:02:52 +0800

    Add support for Darwin.

--- Python-pip-source-changer.py
+++ Python-pip-source-changer.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 import platform
 import os
 
+
 def mkdir(path):
-    path=path.strip().rstrip("\\")
-    isExists=os.path.exists(path)
+    isExists = os.path.exists(path)
     if not isExists:
-        os.makedirs(path) 
-        print(path+' Create finish/')
+        os.makedirs(path)
+        print(path + ' Create finish.')
         return True
     else:
-        print(path+' Directory already exists.')
+        print(path + ' Directory already exists.')
         return False
 
-def mkfile(filepath):
+
+def mkfile(filePath):
     pipfile = "[global]\ntrusted-host=mirrors.aliyun.com\nindex-url=http://mirrors.aliyun.com/pypi/simple/"
-    if os.path.exists(filepath):
+    if os.path.exists(filePath):
         if str(input("File exist!Cover?(Y/N))")).upper() == 'N':
             print("Not Cover.")
             return
-    with open(filepath, 'w') as fp:
+    with open(filePath, 'w') as fp:
         fp.write(pipfile)
-    print("Wirte finish.")
+    print("Write finish.")
 
-def platformSelect( ):
-    sysstr = platform.system()
-    if(sysstr =="Windows"):
-        print("System type: Windows")
-        path=os.getenv('HOMEPATH')+"\\pip"
+
+def platformSelect():
+    systype = platform.system()
+    print("System type: " + systype)
+    if systype == "Windows":
+        path = os.getenv('HOMEPATH') + "\\pip"
         mkdir(path)
-        mkfile(path+'\\pip.ini')
-        
-    elif(sysstr == "Linux"):
-        print("System type: Linux")
-        path="~/.pip/"
+        mkfile(path + '\\pip.ini')
+
+    elif systype == "Linux" or systype == "Darwin":
+        path =os.path.expandvars('$HOME')+"/.pip"
         mkdir(path)
-        mkfile(path+'\\pip.conf')
+        mkfile(path + '/pip.conf')
     else:
-        print ("Other System tasks")
+        print("System type: " + systype)
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     platformSelect()
